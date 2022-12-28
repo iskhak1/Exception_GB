673098774067a747de196ab60e3ad3a545eb0894
Iskhak <isakask12242001@gmail.com>
2022-12-28 10:11:47 +0300

    ex2__2 fix code

--- src/main/java/ru/gb/ex2_2.java
+++ src/main/java/ru/gb/ex2_2.java
@@ -1,5 +1,7 @@
 package ru.gb;
 
+import java.io.FileNotFoundException;
+
 public class ex2_2 {
 
     public static void main(String[] args) throws Exception {
@@ -8,14 +10,14 @@ public class ex2_2 {
             int b = 3;
             System.out.println(a / b);
             printSum(23, 234);
-            int[] abc = { 1, 2 };
+            int[] abc = {1, 2};
             abc[3] = 9;
+        }catch (NullPointerException ex) {
+                System.out.println("Указатель не может указывать на null!");
+        } catch (IndexOutOfBoundsException ex) {
+                System.out.println("Массив выходит за пределы своего размера!");
         } catch (Throwable ex) {
             System.out.println("Что-то пошло не так...");
-        } catch (NullPointerException ex) {
-            System.out.println("Указатель не может указывать на null!");
-        } catch (IndexOutOfBoundsException ex) {
-            System.out.println("Массив выходит за пределы своего размера!");
         }
     }
     public static void printSum(Integer a, Integer b) throws FileNotFoundException {
